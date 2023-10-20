4628a1de2a38e5437ea18497b400125f347276ee
David Archer <16766645+davidaustinarcher@users.noreply.github.com>
2023-10-20 15:13:38 +0100

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker tag nodegoat:1.3.0 contrastsecuritydemo/nodegoat:1.3.0
 docker push contrastsecuritydemo/nodegoat:1.3.0
 #With v4
 docker tag nodegoat-assess:1.3.0 contrastsecuritydemo/nodegoat-assess:1.3.0
-docker push contrastsecuritydemo/nodegoat-asssess:1.3.0
+docker push contrastsecuritydemo/nodegoat-assess:1.3.0
 #With v5
 docker tag nodegoat-protect:1.3.0 contrastsecuritydemo/nodegoat-protect:1.3.0
 docker push contrastsecuritydemo/nodegoat-protect:1.3.0
